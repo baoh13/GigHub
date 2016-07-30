0430685d20e00b7774ed346e1af4382cb176b78e
baoh13 <dzacu1a@gmail.com>
2016-07-30 16:28:58 +0100

    Extend the viewmodel and register form with Name property

--- GigHub/ViewModels/RegisterViewModel.cs
+++ GigHub/ViewModels/RegisterViewModel.cs
@@ -5,6 +5,11 @@ namespace GigHub.ViewModels
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
